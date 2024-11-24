40c43866234311786a74331b62063c03b8aabb0a
Nicácio Teixeira <nicacio.maia@gmail.com>
2024-11-24 02:45:45 -0300

    Adicionando novo componente SOBRE

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,12 +3,13 @@ import { RouterModule, RouterLink, RouterOutlet } from '@angular/router';
 import { HeaderComponent } from './header/header.component';
 import { FooterComponent } from './footer/footer.component';
 import { CommonModule } from '@angular/common';
+import { SobreComponent } from "./sobre/sobre.component";
 
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterModule, HeaderComponent, FooterComponent, CommonModule, RouterLink, RouterOutlet],
+  imports: [RouterModule, HeaderComponent, FooterComponent, CommonModule, RouterLink, RouterOutlet, SobreComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
