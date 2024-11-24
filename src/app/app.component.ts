8590ec8a622939c3fe9bc2fa8f9e0258afcf19c8
Nicácio Teixeira <nicacio.maia@gmail.com>
2024-11-24 11:47:42 -0300

    Criando componetes: EspaçoKids, Contato, e atualizações de estilos

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -4,12 +4,14 @@ import { HeaderComponent } from './header/header.component';
 import { FooterComponent } from './footer/footer.component';
 import { CommonModule } from '@angular/common';
 import { SobreComponent } from "./sobre/sobre.component";
+import { EspacoKidsComponent } from "./espaco-kids/espaco-kids.component";
+import { ContatoComponent } from "./contato/contato.component";
 
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterModule, HeaderComponent, FooterComponent, CommonModule, RouterLink, RouterOutlet, SobreComponent],
+  imports: [RouterModule, HeaderComponent, FooterComponent, CommonModule, RouterLink, RouterOutlet, SobreComponent, EspacoKidsComponent, ContatoComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,9 +2,11 @@ import { Routes } from '@angular/router';
 import { NoticiaComponent } from './noticia/noticia.component';
 import { DoacaoComponent } from './doacao/doacao.component';
 
+
 export const routeConfig: Routes = [
     {path: '', component: NoticiaComponent, title: "Notícias"},
     {path: 'doacao', component: DoacaoComponent, title:"Doações"}
+    
 ];
 
 export default routeConfig;
